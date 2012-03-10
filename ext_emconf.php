4014f57fa3399a77e068833f94d89e6b384a038f
groeken <groeken@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-10 18:04:58 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cs_powermail_limit/trunk@59113 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cs_powermail_limit".
 #
-# Auto generated 12-02-2012 22:56
+# Auto generated 10-03-2012 17:19
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,40 +11,38 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Powermail Limit - modified',
+	'title' => 'Powermail Limit',
 	'description' => 'Makes it possible to set a Limit to Powermail.',
 	'category' => 'fe',
-	'shy' => 0,
-	'version' => '10.0.2',
+	'author' => 'Christian Strunk',
+	'author_email' => 'Christian.M.Strunk@googlemail.com',
+	'shy' => '',
 	'dependencies' => 'cms,powermail',
 	'conflicts' => '',
 	'priority' => '',
-	'loadOrder' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
+	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Christian Strunk',
-	'author_email' => 'Christian.M.Strunk@googlemail.com',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
 			'powermail' => '',
-),
+		),
 		'conflicts' => array(
-),
+		),
 		'suggests' => array(
-),
-),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"5ce9";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"a73b";s:12:"ext_icon.gif";s:4:"ad4f";s:17:"ext_localconf.php";s:4:"a47c";s:14:"ext_tables.php";s:4:"fc3f";s:14:"ext_tables.sql";s:4:"265c";s:16:"locallang_db.xml";s:4:"b6ea";s:20:"locallang_db_old.xml";s:4:"23e3";s:19:"doc/wizard_form.dat";s:4:"fe3c";s:20:"doc/wizard_form.html";s:4:"9a4f";s:35:"doc/manual.rtfd/Pasted Graphic.tiff";s:4:"fcb9";s:23:"doc/manual.rtfd/TXT.rtf";s:4:"6664";s:37:"pi1/class.tx_cspowermaillimit_pi1.php";s:4:"fbca";}',
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"24bb";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"c914";s:12:"ext_icon.gif";s:4:"ad4f";s:17:"ext_localconf.php";s:4:"2006";s:14:"ext_tables.php";s:4:"af80";s:14:"ext_tables.sql";s:4:"c034";s:71:"flexform_field.powermailinformation_tx_cspowermaillimit_pi_flexform.xml";s:4:"5081";s:55:"flexform_tt_content_tx_cspowermaillimit_pi_flexform.xml";s:4:"f457";s:16:"locallang_db.xml";s:4:"95df";s:19:"doc/wizard_form.dat";s:4:"0a37";s:20:"doc/wizard_form.html";s:4:"8378";s:37:"pi1/class.tx_cspowermaillimit_pi1.php";s:4:"334b";s:17:"pi1/locallang.xml";s:4:"76c6";}',
 	'suggests' => array(
-),
+	),
 );
 
 ?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,9 +5,8 @@ if (!defined('TYPO3_MODE')) {
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_cspowermaillimit_pi1.php', '_pi1', '', 1);
 
+// Hook form Limit-Ext
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MainContentHookAfter'][] = 'EXT:cs_powermail_limit/pi1/class.tx_cspowermaillimit_pi1.php:tx_cspowermaillimit_pi1';
-
-// add hook for free places
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_FieldHook']['tx_cspowermaillimit_freeplaces'][] = 'EXT:cs_powermail_limit/pi1/class.tx_cspowermaillimit_pi1.php:tx_cspowermaillimit_pi1';
-
+// Hook for Limit Information
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_FieldHook']['tx_cspowermaillimit_limitinformation'][] = 'EXT:cs_powermail_limit/pi1/class.tx_cspowermaillimit_pi1.php:tx_cspowermaillimit_pi1';
 ?>
