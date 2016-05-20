b1ff9e9ed75f6ec4200de25068527d595f50add6
Jakob Berlin <j.berlin@ewerk.com>
2016-05-20 15:32:12 +0200

    Updated dependency version reference.

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array(
         array(
             'depends' =>
                 array(
-                    'typo3' => '>=7.6',
+                    'typo3' => '>=7.6.0',
                     'powermail' => '>=2.25.2'
                 ),
             'conflicts' =>
